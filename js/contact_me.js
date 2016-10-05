90722d69c1c0ec670ed5d95c2caccc55b301716f
Sahif ali <sahif_ali@hotmail.co.nz>
2016-10-06 02:34:36 +1300

    Revert "Contact Form Update 1.0"
    This reverts commit c2fac05e6a284d7d2321acef91295483542b05bd.

--- js/contact_me.js
+++ js/contact_me.js
@@ -8,15 +8,14 @@ $contactForm.submit(function(e) {
 		dataType: 'json',
 		beforeSend: function() {
 			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
-		
 		},
 		success: function(data) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.replace('<div class="alert alert--success">Message sent!</div>');
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
 		},
 		error: function(err) {
 			$contactForm.find('.alert--loading').hide();
-			$contactForm.replace('<div class="alert alert--error">Ops, there was an error.</div>');
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
 		}
 	});
 });
